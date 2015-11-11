2ecc300e7c8aa59ae9c38ee51654198e0f0f53b8
Luis Claudio <lrodrigu@tulane.edu>
2015-11-10 21:26:32 -0500

    Finished homework 6

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110022438) do
+ActiveRecord::Schema.define(version: 20151110235952) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
+    t.string "name"
   end
 
 end
--- sinatra_blog.rb
+++ sinatra_blog.rb
@@ -5,7 +5,13 @@ set :database, "sqlite3:database.sqlite3"
 
 require './user'
 
-get '/' do
+get '/users' do
 	@users = User.all
 	erb :index
+end
+
+
+get '/users/:id' do
+	@user = User.find_by_id(params[:id])
+	erb :show
 end
